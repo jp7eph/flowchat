0b180eef1e41c7e0fceef4c7d236882d5d232ab4
jp7eph <jp7eph@gmail.com>
2022-01-24 02:00:55 +0900

    wip

--- flowchaat_app/src/render.js
+++ flowchaat_app/src/render.js
@@ -9,15 +9,16 @@ window.onload = function () {
 
 async function flow_chat(text) {
   let p_element = document.createElement('p');
-   //初期状態の横方向の位置は画面の右端に設定
+   //　初期状態の横方向の位置は画面の右端に設定
   p_element.style.left = (document.documentElement.clientWidth) + 'px';
   var random = Math.round(Math.random() * document.documentElement.clientHeight);
-  //初期状態の縦方向の位置は画面の上端から下端の間に設定（ランダムな配置に）
+  // 初期状態の縦方向をランダムに
   p_element.style.top = random + 'px';
 
   p_element.appendChild(document.createTextNode(text));
   container.appendChild(p_element);
 
+  // アニメーションが終了したら要素削除
   p_element.addEventListener('animationend', () => {
     p_element.parentNode.removeChild(p_element);
   });
